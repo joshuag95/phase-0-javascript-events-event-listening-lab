eacac1a6a7a2f0c45630b4e848fba88f867f5bc8
Joshua Greer <jtgreer95@gmail.com>
2022-06-28 20:25:18 -0500

    commit tree 59c04c89563a232240198afafd28cbad811b19b4

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function addingEventListener() {
-    const input = document.getElementById('button');
+  const input = document.getElementById('button');
 
-function clickAlert() {
-  alert('I was clicked!');
-}
-
-input.addEventListener('click', clickAlert);
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  input.addEventListener('click', clickAlert);
+  
 }
